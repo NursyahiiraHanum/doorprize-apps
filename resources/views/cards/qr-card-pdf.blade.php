84786e5206aaab2d63cc0da81a770885c15c18bd
NursyahiiraHanum <nursyahiirahanum@gmail.com>
2026-09-20 18:03:54 +0700

    yaa

--- resources/views/cards/qr-card-pdf.blade.php
+++ resources/views/cards/qr-card-pdf.blade.php
@@ -29,7 +29,7 @@
         }
         .ticket-table {
             width: 100%;
-            height: 100%;
+            height: 206pt
             border-collapse: collapse;
             table-layout: fixed;
         }
